2a2edaa5ecb6ab3b35d305f4634a617248126529
rafabbroaya@hotmail.com <!ChangePwd@3001>
2018-04-09 16:39:39 -0500

    servlet approach

--- src/main/java/com/rsanchezg/inventory/InventoryApplication.java
+++ src/main/java/com/rsanchezg/inventory/InventoryApplication.java
@@ -2,25 +2,40 @@ package com.rsanchezg.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 
 @SpringBootApplication
-public class InventoryApplication extends WebSecurityConfigurerAdapter {
+public class InventoryApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(InventoryApplication.class, args);
   }
 
   @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.cors().and().authorizeRequests().anyRequest().authenticated();
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(InventoryApplication.class);
   }
 
   @Bean
   HeaderHttpSessionStrategy sessionStrategy() {
     return new HeaderHttpSessionStrategy();
+  }
+
+  @Configuration
+  @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http.cors().and().authorizeRequests().anyRequest().authenticated();
+    }
   }
 }
